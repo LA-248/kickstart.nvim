f76f9d71b95ccdc0d2cfe17732567b552884b0f5
Luca Antunes <104241771+LA-248@users.noreply.github.com>
2026-07-14 23:13:00 +0200

    feat: update config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -122,4 +122,29 @@ return {
       },
     },
   },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      vim.keymap.set('n', '<leader>ha', function()
+        harpoon:list():add()
+      end, { desc = '[H]arpoon [A]dd file' })
+
+      vim.keymap.set('n', '<leader>hh', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = '[H]arpoon menu' })
+
+      vim.keymap.set('n', '<leader>h1', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon file 1' })
+
+      vim.keymap.set('n', '<leader>h2', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon file 2' })
+    end,
+  },
 }
