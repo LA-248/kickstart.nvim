38bf76ccb7e7c768cfd4f3d418dc323d1d7a2bb1
Luca Antunes <104241771+LA-248@users.noreply.github.com>
2026-07-19 17:30:14 +0200

    feat: update config

--- init.lua
+++ init.lua
@@ -189,8 +189,8 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-S-A-D-?>', '<C-w><C-h>', { desc = 'Move focus to the left window (Hyper+?)' })
+vim.keymap.set('n', '<C-S-A-D-,>', '<C-w><C-l>', { desc = 'Move focus to the right window (Hyper+,)' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
@@ -280,6 +280,11 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
+      -- Keep WhichKey available for command prefixes, but do not open it
+      -- automatically when entering Visual mode with `v`.
+      triggers = {
+        { '<auto>', mode = 'nso' },
+      },
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
